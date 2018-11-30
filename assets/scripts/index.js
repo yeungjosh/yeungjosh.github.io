99a4bda2845e5d2d001ae825799381af882e400e
Josh Yeung <yeungj@berkeley.edu>
2018-11-29 23:19:58 -0800

    svgs, js, and other fun things

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,6 @@
 
 window.addEventListener("load", function() {
+    // hehehe im not using jquery hehehe take dat wdd
     const $overlay = document.querySelector('.overlay')
     $overlay.classList.add('fade-out')
     setTimeout(function() {
