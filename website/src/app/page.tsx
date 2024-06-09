0c06d66e1e8b81637b42fbc777838a798eee339a
Joshua Yeung <yeungj@berkeley.edu>
2024-06-09 02:03:14 -0700

    Revert "Revert "Adding personal website.""
    This reverts commit f18aacf1249ce9dcab43a6936a9e02ad3e0c11f1.
    
    Redo

---
+++ website/src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from '../components/component';
+
+const Page = () => {
+  return (
+    <div>
+      <Component />
+    </div>
+  );
+};
+
+export default Page;
